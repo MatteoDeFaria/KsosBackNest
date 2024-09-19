467fa6da0868f6d2b6b2b29276f495ff417a47bc
Matteo <m.defaria@wanadoo.fr>
2024-09-19 03:17:25 +0200

    [update]: remove console.log

--- src/request/request.interceptor.ts
+++ src/request/request.interceptor.ts
@@ -11,10 +11,9 @@ import { MetricsService } from 'src/metrics/metrics.service';
 @Injectable()
 export class RequestInterceptor implements NestInterceptor {
   constructor(private metricsService: MetricsService) {}
-  
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     this.metricsService.incrementCounter();
-    console.log(context.getType)
     return next.handle();
   }
 }
