60cae2ba2ac686920960a7f6890f7d8c085d8d5c
Matteo <m.defaria@wanadoo.fr>
2025-01-06 13:26:43 +0100

    [update]: update every hour the profile icon and the summoner level

---
+++ src/tasks/entities/lol-user.entities.ts
@@ -0,0 +1,8 @@
+export class LolUserEntities {
+  id: string;
+  accountId: string;
+  puuid: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
+import { LolUserEntities } from './entities/lol-user.entities';
 
 @Injectable()
 export class TasksService {
@@ -93,5 +94,36 @@ export class TasksService {
     });
 
     this.logger.debug(`Get TFT Ranked data is called when the minute is 0`);
+  }
+
+  @Cron('0 * * * *')
+  async getDataProfileIcon() {
+    const users: Prisma.LeagueUserCreateInput[] =
+      await this.prisma.leagueUser.findMany();
+
+    users.forEach(async (element: Prisma.LeagueUserCreateInput) => {
+      const getUserData: LolUserEntities = await firstValueFrom(
+        this.httpService
+          .get(
+            `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${element.puuid}?api_key=${process.env.RIOT_API_KEY}`,
+          )
+          .pipe(
+            catchError((error: AxiosError) => {
+              this.logger.error(error.response.data);
+              throw 'An error happened to fetch LoL!';
+            }),
+          ),
+      ).then((res) => res.data);
+
+      await this.prisma.leagueUser.update({
+        where: { id: element.id },
+        data: {
+          profileIconId: getUserData.profileIconId,
+          summonerLevel: getUserData.summonerLevel,
+        },
+      });
+    });
+
+    this.logger.debug(`Get LoL user data is called when the minute is 0`);
   }
 }
